45ac66fe39b68fd5f04190942766b65c46f77df9
Thomas D'Ostilio <tdostilio@gmail.com>
2025-12-10 19:29:51 -0500

    updates boilerplate

--- src/controllers/invoiceController.js
+++ src/controllers/invoiceController.js
@@ -1,33 +1,26 @@
 const Invoice = require("../models/Invoice")
-const logger = require("../utils/logger")
 const handleControllerError = require("../utils/handleControllerError")
 
 // @desc    Get all invoices
 // @route   GET /api/invoices
-// @access  Public
 const getAllInvoices = async (req, res) => {}
 
 // @desc    Get single invoice
 // @route   GET /api/invoices/:id
-// @access  Public
 const getInvoice = async (req, res) => {}
 
 // @desc    Create new invoice
 // @route   POST /api/invoices
-// @access  Public
 const createInvoice = async (req, res) => {
-  logger.info("hit method")
-  return res.status(201).send({ success: true, invoice: {} })
+  return res.status(201).send({ invoice: {} })
 }
 
 // @desc    Update invoice
 // @route   PATCH /api/invoices/:id
-// @access  Public
 const updateInvoice = async (req, res) => {}
 
 // @desc    Delete invoice
 // @route   DELETE /api/invoices/:id
-// @access  Public
 const deleteInvoice = async (req, res) => {}
 
 module.exports = {
